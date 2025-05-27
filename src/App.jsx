4639022d0a74e0dddc4940f6a0046a0c5e1a7579
ltherisod <lauratherisod@gmail.com>
2025-05-27 18:42:19 -0300

    add photo and clean up app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
 import './App.css'
-//importar el css 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListContainer'
-import NavBar from './components/NavBar'
-import ButtonMultiuso from "./examples/ButtonMultiuso"
-import Contador from './examples/Contador'
 import NavBarBootstrap from './components/NavBarBootstrap';
 function App() {
-//  const styles = {
-//   backgroundColor:'yellow',
-//   padding:'1rem',
-//   color:'black',
-//   fontWeight:'bold'
-//  }
 
-//  const saludar = () =>{
-//   alert('Hola!')
-//  }
 
   return (
     <>
     <NavBarBootstrap/>
-    {/* <NavBar/> */}
     <ItemListContainer saludo='Bienvenidos a mi app'/>
-     {/* <Contador/>
-      <ButtonMultiuso texto={'Hace Click!'} styles={styles} handler={saludar}/>
-      <ButtonMultiuso texto='Otro boton' cssStyle='mi-boton'/> */}
     </>
   )
 }
