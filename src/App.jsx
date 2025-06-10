5a14b2130404934acc8fc24f6c3ed8fdf831bdb3
ltherisod <lauratherisod@gmail.com>
2025-06-10 18:45:09 -0300

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListContainer'
 import NavBarBootstrap from './components/NavBarBootstrap';
-import FetchCountry from './examples/FetchCountry';
-import FetchApi from './examples/FetchApi';
-import withLogging from './hocs/withLogging';
-import MiComponente from './examples/MiComponente';
 import ItemDetailContainer from './components/ItemDetailContainer';
 
 function App() {
-const MiComponenteConHoc = withLogging(MiComponente)
+
 
   return (
     <>
     <NavBarBootstrap/>
-    {/* <FetchCountry/>
-    <FetchApi/>
-    <MiComponenteConHoc mensaje='Holis miren estoy usando un HOC 🤭'/>
-    <MiComponente mensaje='No estoy usando un HOC '/> */}
-    {/* <ItemListContainer saludo='Bienvenidos a mi app'/> */}
+    <ItemListContainer saludo='Bienvenidos a mi app'/>
     <ItemDetailContainer/>
     </>
   )
--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState} from 'react'
 
 const ItemCount = ({stock}) => {
-    // console.log(props.stock)
     const [count, setCount] =useState(1)
-    const [compra, setCompra]= useState(false)
     const sumar = ()=>{
         if(count < stock){
 
@@ -16,36 +14,17 @@ const ItemCount = ({stock}) => {
             setCount(count -1)
         }
     }
-    const comprarItem = () => {
-        setCompra(!compra)
-    }
-
-    //No se recomienda 
-    useEffect(()=>{
-        console.log('Sin el array de dependencias me ejecuto SIEMPRE')
-    })
-
-    //Es el mas usado
-     useEffect(()=>{
-        console.log('Con el array de dependencias vacio, me ejecuto UNA VEZ')
-    },[])
-
-    //Se usa en casos especificos
-      useEffect(()=>{
-        console.log('Con el array de dependencias con datos, me ejecuto cuando se monta el componente y cada vez que lo que esta escuchando se actualice')
-    },[compra])
-
-    console.log('soy item count')
+   
 
 
   return (
-    <div>
+    <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
         <div>
         <button className='btn btn-danger' onClick={restar}>-</button>
         <span className='btn'>{count}</span>
         <button className='btn btn-success' onClick={sumar}>+</button>
     </div>
-    <button className='btn btn-primary' onClick={comprarItem} disabled={stock === 0}>Comprar</button>
+    <button className='btn btn-primary'  disabled={stock === 0}>Comprar</button>
     </div>
   )
 }
